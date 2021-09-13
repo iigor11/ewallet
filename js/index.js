4ee1f35abdd20f3ac02e010e3b814523a88ccd06
Igor Rabelo <igorrabelo04@icloud.com>
2021-09-13 17:27:54 -0300

    feat: icon in category

--- js/index.js
+++ js/index.js
@@ -1,78 +1,133 @@
-var transactions = [
-    {
-        title: "Lacoste - Barra Shopping",
-        price: 25000,
-        currency: "BRL",
-        type: "saída",
-        category: "venda",
-        date: "13/08/2021"
-    },
-    {
-        title: "PIX recebido",
-        price: 7500,
-        currency: "BRL",
-        type: "entrada",
-        category: "Transferência",
-        date: "13/08/2021"
-    }
-]
+var icons = {
+  "Alimentação": "coffee",
+  "Compra": "shopping-cart",
+  "Saúde": "heart",
+  "Transporte": "map-pin",
+  "Educação": "book",
+  "Pagamento": "dollar-sign",
+  "Investimento": "pie-chart"
+}
+// transações ficticias cadastradas (criadas manualmente)
+var transactions = 
+JSON.parse(localStorage.getItem("@ewallet/transactions"))||[];
 
-{/*
-    <tr>
-        <td>Lacoste - Barra Shopping</td>
-        <td>R$ 25.000,00</td>
-        <td class="red"><i data-feather="dollar-sign"></i> Venda</td>
-        <td>13/08/2021</td>
-    </tr>
-*/}
+
+// Adiciona o corpo da tabela na variavel table
 var table = document.querySelector("#table tbody");
 
-transactions.map(transaction => {
-    var row = document.createElement("tr");
+//Mapeamento das transações
+transactions.map((transaction) => {
+  var row = document.createElement("tr");
+  // <tr>
+  //  <td>Desenvolvimento</td>
+  // </tr>
 
-    var title = document.createElement("td");
-    title.append(transaction.title);
+  var title = document.createElement("td");
+  title.append(transaction.title);
 
-    var price = document.createElement("td");
-    var value = moneyFormat(transaction.currency, transaction.price)
-    price.append(value);
-    
-    var category = document.createElement("td");
-    category.append(transaction.category); 
-    
-    var date = document.createElement("td");
-    date.append(transaction.date); 
-    
+  var price = document.createElement("td");
+  var value = moneyFormat(transaction.currency, transaction.price);
+  price.append(value);
 
-    row.appendChild(title)
-    row.appendChild(price)
-    row.appendChild(category)
-    row.appendChild(date)
+  var category = document.createElement("td");
+  category.classList.add(`${transaction.category==="entrada"?"green":"red"}`)
+  // i data-fearther="dolar-sing"
+  var icon = document.createElement("i");
+  icon.setAttribute("data-feather", icons[transaction.identifier])
+  category.appendChild(icon)
+  category.append(transaction.identifier);
 
-    table.appendChild(row)
-})
+  var date = document.createElement("td");
+  date.append(transaction.date);
 
+  row.appendChild(title);
+  row.appendChild(price);
+  row.appendChild(category);
+  row.appendChild(date);
+
+  table.appendChild(row);
+});
 
 var addBtn = document.querySelector("#addButton a");
 var popup = document.querySelector("#popupbackground");
 var closeBtn = document.querySelector("#popup form a");
+var form = document.querySelector("form");
 
-addBtn.addEventListener("click", ()=>{
-    // O que vai acontece quando clicar no botão adicionar
-    popup.style.display = "flex";
-    popup.style.transition = "display 5s"; 
-})
+addBtn.addEventListener("click", () => {
+  // O que vai acontecer quando clicar no botão adicionar
+  popup.style.display = "flex";
+  popup.style.transition = "display 5s";
+});
 
-closeBtn.addEventListener("click", ()=>{
-    popup.style.display = "none";
-    popup.style.transition = "display 5s";
-})
+closeBtn.addEventListener("click", () => {
+  // O que vai acontecer quando clicar no botão fechar
+  popup.style.display = "none";
+  popup.style.transition = "display 5s";
+  form.reset();
+});
 
+form.addEventListener("submit", (event) => {
+  //Previne para que a tela não recarregue
+  event.preventDefault();
 
-function moneyFormat(currency, price){
-    var value = new Intl.NumberFormat("pt-BR", {
-        style: "currency",
-        currency: currency
-    }).format(price);
-    return value;
+  //Capturar os dados preenchidos no formulário
+  var data = new FormData(event.target);
+
+  //Desestrururar os dados em variáveis
+  var { title, price, category, currency, identifier } =
+    Object.fromEntries(data);
+  //Pegar a data atual no formato DD/MM/YYYY
+  var date = new Date().toLocaleDateString();
+
+  var transaction = {
+    title,
+    price: parseFloat(price),
+    category,
+    currency, 
+    identifier,
+    id: transactions.length + 1,
+    date
+  }
+  transactions.push(transaction)
+  localStorage.setItem("@ewallet/transactions", JSON.stringify(transactions));
+  window.location.reload();
+});
+
+var entrada = document.querySelector(".in h1")
+var saida = document.querySelector(".out h1")
+var total = document.querySelector(".total h1")
+
+var valoresEntrada = transactions.reduce((count, currentValue) => {
+if (currentValue.category === "entrada"){
+    return count + currentValue.price;
+}else {
+    return count;
 }
+}, 0)
+
+var valoresSaida = transactions.reduce((count, currentValue) => {
+if (currentValue.category === "saida"){
+    return count + currentValue.price;
+}else {
+    return count;
+}
+}, 0)
+
+var somatorio = valoresEntrada - valoresSaida
+
+entrada.innerHTML = moneyFormat("BRL", valoresEntrada)
+saida.innerHTML = moneyFormat ("BRL", valoresSaida)
+total.innerHTML = moneyFormat ("BRL", somatorio)
+
+// Métodos ou Funções
+function moneyFormat(currency, price) {
+  var value = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency,
+  }).format(price);
+  return value;
+}
+
+
+
+feather.replace();
